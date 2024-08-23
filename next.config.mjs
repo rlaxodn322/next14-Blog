06e134b570f6142149e012bcd7cdc1537d94c9c6
taewoo <rlaxodn322@gmail.com>
2024-08-23 11:14:27 +0900

    pwa test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 기존 Next.js 설정
+};
+
+export default withPWA({
+  dest: 'public',
+})(nextConfig);
